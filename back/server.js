c91ab4ab3e6909c3cfeeb6fd1b4fe6ee0ba5cd01
Maxime <maxime.sausset@hotmail.fr>
2023-05-15 14:27:27 +0200

    creation repertoire front

---
+++ back/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/user.routes");
+const contactRoutes = require("./routes/contact.routes");
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db");
+const cors = require("cors");
+const app = express();
+
+// Autorisation cors
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+};
+
+app.use(cors(corsOptions));
+
+// req.params = url / req.body = corps du fichier / req.cookie = accès aux cookies
+// Ou express.json() / express.urlencoded ==> Supporter par express nativement maintenant
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// routes
+
+app.use("/api/user", userRoutes);
+app.use("/api/contact", contactRoutes);
+
+// serveur
+
+app.listen(process.env.PORT, () => {
+  console.log(`Demarré au port ${process.env.PORT}`);
+});
